8b399c88a00c21d65033c09f78c6ad9186c55fae
DanielRamos <devdsfr@gmail.com>
2022-08-28 16:02:42 -0300

    solved

--- ManagerRecords/Model/Recordes.cs
+++ ManagerRecords/Model/Recordes.cs
@@ -2,7 +2,7 @@
 {
     public class Recordes
     {
-        public int fgId { get; set; }
+        public int Id { get; set; }
         public string? UserName { get; set; }
         public int Age { get; set; }
         public Double Number { get; set; }
